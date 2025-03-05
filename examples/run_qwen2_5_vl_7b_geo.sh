1fe18d4569fa13bf1a951c0d0615ab5d04ff8efd
Zhang Yizhen <113516384+alchemistyzz@users.noreply.github.com>
2025-03-05 10:33:50 +0800

    Update run_qwen2_5_vl_7b_geo.sh

--- examples/run_qwen2_5_vl_7b_geo.sh
+++ examples/run_qwen2_5_vl_7b_geo.sh
@@ -3,6 +3,10 @@ set -x
 export VLLM_ATTENTION_BACKEND=XFORMERS
 
 MODEL_PATH=Qwen/Qwen2.5-VL-7B-Instruct  # replace it with your local file path
+export WANDB_BASE_URL=https://api.wandb.ai
+export WANDB_PROJECT=verl_test
+export WANDB_API_KEY=19e2bb17296ca54e3b6de27ef184eac2eb7efd5f  # 确保 API Key 正确
+export WANDB_RUN_NAME=Qwen-VL2_5-7B-GRPO-$(date +%Y-%m-%d-%H-%M-%S)
 
 python3 -m verl.trainer.main \
     config=examples/grpo_example.yaml \
@@ -11,4 +15,4 @@ python3 -m verl.trainer.main \
     worker.actor.model.model_path=${MODEL_PATH} \
     worker.rollout.enable_chunked_prefill=false \
     trainer.experiment_name=qwen2_5_vl_7b_geo \
-    trainer.n_gpus_per_node=4
+    trainer.n_gpus_per_node=8
